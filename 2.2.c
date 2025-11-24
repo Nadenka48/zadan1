50925d260d333e9e061b49faa55ec266be41eefc
Комова Надежда ТКИ-141 <komovanadenka@yandex.ru>
2025-11-24 15:04:48 +0300

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,76 +1,47 @@
-﻿#include <math.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <float.h>
+#pragma warning(disable:4996)
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 
 /**
- * @brief Функция расчитывающая значение y при x > a
- * @param x Входная величина
- * @return Значение функции
+ *@brief считывает значение, введенное с клавиатуры с проверкой ввода
+ *@return считанное значение
  */
-double get_y_1(double x);
+double getValue();
 
 /**
- * @brief Функция расчитывающая значение y при x <= a
- * @param x Входная величина
- * @return Значение функции
+ *@brief Функция, которая расчитывается по заданной формуле
+ *@param x - введённое значение x, a = константа равная 2
+ *@return рассчитанное значение функции
  */
-double get_y_2(double x);
+double getF(const double x, const double a);
 
-/**
- * @brief Функция принимающая и проверяющая значение на ввод
- * @return Значение
- */
-double get_value();
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const double a = 2;
+	printf("Введите значение х: ");
+	double x = getValue();
+	printf("Функция у = %.5lf", getF(x, a));
+	return 0;
+}
 
-/**
-* @brief Точка входа в программу
-* @return Возврящает 0, если программа работает верно, иначе 1
-*/
-int main()
+double getValue()
 {
-    double x;
-    puts("Enter x: ");
-    x = get_value();
-    double result = 0;
-    const int a = 5;
-    if (x > a)
-    {
-        result = get_y_1(x);
-    }
-    else if (x <= a)
-    {
-        result = get_y_2(x);
-    }
-    else
-    {
-        errno = EIO;
-        perror("Wrong value");
-        return 1;
-    }
-    printf("%lf", "Результат: ", result);
-    return 0;
+	double value = 0;
+	if (!scanf("%lf", &value))
+	{
+		printf("Error");
+		abort();
+	}
+	return value;
 }
-double get_y_1(double x)
+
+double getF(const double x, const double a)
 {
-    double y = tan(x) + (M_PI / 3.0);
-    return y;
-}
-double get_y_2(double x)
-{
-    double y = sin(x) * a;
-    return y;
-}
-double get_value()
-{
-    double a;
-    int res = scanf_s("%lf", &a);
-    if (res != 1)
-    {
-        errno = EIO;
-        perror("Wrong value");
-        abort();
-    }
-    return a;
+	if (x > a)
+	{
+		return tan(x) + M_PI / 3;
+	}
+	return sin(x) * a;
 }
